e2b9bd8f575408c2efce3b3c50036a629da269c4
rynp03 <aryan.ritesh002@gmail.com>
2026-05-05 16:29:53 +0530

    v1.0 Bug fixes

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -3,27 +3,14 @@ import { View } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Login from "../screens/auth/Login";
 import Dialler from "../screens/mains/Dialler";
-import { useAuthStore } from "../store/authStore";
-import { loadSession } from "../utils/secureStorage";
 
 const Stack = createStackNavigator();
 
 export default function RootNavigator() {
   const [initialRoute, setInitialRoute] = useState(null);
-  const setTokens = useAuthStore((s) => s.setTokens);
-  const setSipCredentials = useAuthStore((s) => s.setSipCredentials);
 
   useEffect(() => {
-    (async () => {
-      const session = await loadSession();
-      if (session?.tokens && session?.sipCredentials) {
-        setTokens(session.tokens);
-        setSipCredentials(session.sipCredentials);
-        setInitialRoute("Dialler");
-      } else {
-        setInitialRoute("Login");
-      }
-    })();
+    setInitialRoute("Login");
   }, []);
 
   if (!initialRoute) return <View style={{ flex: 1, backgroundColor: "#fff" }} />;
